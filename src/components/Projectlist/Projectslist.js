6d02daf5fa31b8fe098798e7af7b79133e03f7de
MikeK1639 <126822125+MikeK1639@users.noreply.github.com>
2023-06-03 17:19:27 -0400

    got rid of unused imports

--- src/components/Projectlist/Projectslist.js
+++ src/components/Projectlist/Projectslist.js
@@ -2,12 +2,7 @@ import React from 'react'
 import '../../pages/style.css';
 import ProjectCard from "./ProjectCard";
 import { Container, Row, Col } from 'react-bootstrap'
-// import useradminpanel from '../../Assets/Admin-Panel-Dashboard.png'
-// import olximage from '../../Assets/olx-clone image.png'
-// import netfliximage from '../../Assets/React-App-netflix-clone.png'
-// import gridlinesbuilders from '../../Assets/Gridlinesbuilders.png'
-// import charlespizza from '../../Assets/Charlestown-Pizza.png'
-// import todolist from '../../Assets/todolist.png'
+
 
 function Projectlist() {
   return (
